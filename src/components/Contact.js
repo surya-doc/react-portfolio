53083e29e3288787a921bad13162a96cacd9ea4c
surya-doc <suryadanabnk@gmail.com>
2021-05-28 22:51:49 +0530

    push updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ function Contact(){
                 <a href={github}><FontAwesomeIcon icon={faLinkedinIn} className="contact-icon" size="2x"/></a>
                 <a href={linkedin}><FontAwesomeIcon icon={faGithub} className="contact-icon" size="2x"/></a>
                 <a href={gmail}><FontAwesomeIcon icon={faEnvelope} className="contact-icon" size="2x"/></a>
-                <p style={{fontSize: "16px", paddingTop: "14px", margin: "0px", letterSpacing: "1px"}}>Made it with <FontAwesomeIcon icon={faHeart} style={{color: "#14fb0a"}}></FontAwesomeIcon> by Surya</p>
+                <p style={{fontSize: "16px", paddingTop: "14px", margin: "0px", letterSpacing: "1px"}}>Made it with <FontAwesomeIcon icon={faHeart} style={{color: "#14fb0a"}}></FontAwesomeIcon> by surya</p>
             </div>
         </div>
     );
